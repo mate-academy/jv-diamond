3fdc9978ad3bc1124d94146f699d38ef8861090c
Sasha192 <sasha192.bunin@gmail.com>
2019-08-19 16:44:13 +0300

    Third try; Oleksandr Bunin Commit 19.09.19

--- src/main/java/core/basesyntax/Diamond.java
+++ src/main/java/core/basesyntax/Diamond.java
@@ -12,7 +12,28 @@ public class Diamond {
      * Вместо System.out.print() или System.out.println() использовать stream.print()
      * или stream.println()
      */
-    public static void drawDiamond(int maxSize, PrintStream stream) {
+    public static void drawDiamond(int n, PrintStream stream) {
+
+    	if( n < 1 || n%2 == 0 ) throw new IllegalArgumentException("maxSize must be odd positive number");
+        StringBuilder all = new StringBuilder(n*n);
+        StringBuilder temp = new StringBuilder(n);
+        int offset = 0; int j = ( n + 1 ) / 2;
+        
+        for(int i = j  -  1; i > 0; i--, j++ ){
+            temp.append(" ".repeat(i));
+            temp.append("*".repeat(j - i));
+            temp.append("\n");
+            all.insert(offset,temp);
+            offset+=temp.length();
+            all.insert(offset,temp);
+            temp.setLength(0);
+        }
+        
+        temp.append("*".repeat(n));
+        temp.append("\n");
+        st.insert(offset,temp);
+        stream.print(all.toString());
+
     }
 
     public static void main(String[] args) {
