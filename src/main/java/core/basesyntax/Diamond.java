9e8df462eb2ca84cdcab55cf7147562436039c67
RomanMinevich <52323566+RomanMinevich@users.noreply.github.com>
2019-08-19 20:33:35 +0300

    Update Diamond.java

--- src/main/java/core/basesyntax/Diamond.java
+++ src/main/java/core/basesyntax/Diamond.java
@@ -13,28 +13,25 @@ public class Diamond {
      * или stream.println()
      */
     public static void drawDiamond(int maxSize, PrintStream stream) {
-      if (maxSize % 2 == 0 || maxSize < 0) {
-      stream.print(null);
-    }
-    String[] asterisks = new String[maxSize/2 + 1];
-    asterisks[0] = "*\maxSize";
-    for(int i = 1; i < asterisks.length; i ++) {
-      asterisks[i] = "**" + asterisks[i - 1];
-    }
-    String[] spaces = new String[maxSize/2 + 1];
-    spaces[0] = "";
-    for(int i = 1; i < spaces.length; i ++) {
-      spaces[i] = " " + spaces[i - 1];
-    }
-    String diamond = "";
-    for (int i = 0; i < maxSize/2 + 1; i ++) {
-      diamond += spaces[spaces.length - i - 1] + asterisks[i];
-    }
-    for (int i = 1; i < n/2 + 1; i ++) {
-      diamond += spaces[i] + asterisks[asterisks.length - i - 1];
-    }
-    stream.print(diamond);
-    }
+        String[] asterisks = new String[maxSize];
+        asterisks[0] = "*";
+        for(int i = 1; i < asterisks.length; i ++) {
+            asterisks[i] = "*" + " " + asterisks[i - 1];
+        }
+        String[] spaces = new String[maxSize];
+        spaces[0] = "";
+        for(int i = 1; i < spaces.length; i ++) {
+            spaces[i] = " " + spaces[i - 1];
+        }
+        String diamond = "";
+        for (int i = 0; i < maxSize; i ++) {
+            diamond += spaces[spaces.length - i - 1] + asterisks[i] + "\n";
+            }
+        for (int i = 1; i <maxSize; i ++) {
+            diamond += spaces[i] + asterisks[asterisks.length - i - 1] + "\n";
+            }
+        stream.print(diamond);
+        }
 
     public static void main(String[] args) {
         drawDiamond(5, System.out);
