722129c63588e1678862da3d8b7c33fba1354bb8
RomanMinevich <52323566+RomanMinevich@users.noreply.github.com>
2019-08-20 00:35:15 +0300

    Update Diamond.java

--- src/main/java/core/basesyntax/Diamond.java
+++ src/main/java/core/basesyntax/Diamond.java
@@ -6,28 +6,26 @@ public class Diamond {
 
     /**
      * Нарисовать ромб со звёздочек заданной величины.
-     *
      * Пример: величина 3, значит максимальное кол-во звёзд в ряду (по диагонали) будет 3.
-     *
      * Вместо System.out.print() или System.out.println() использовать stream.print()
      * или stream.println()
      */
     public static void drawDiamond(int maxSize, PrintStream stream) {
         String[] asterisks = new String[maxSize];
         asterisks[0] = "*";
-        for(int i = 1; i < asterisks.length; i ++) {
+        for (int i = 1; i < asterisks.length; i++) {
             asterisks[i] = "*" + " " + asterisks[i - 1];
         }
         String[] spaces = new String[maxSize];
         spaces[0] = "";
-        for(int i = 1; i < spaces.length; i ++) {
+        for (int i = 1; i < spaces.length; i++) {
             spaces[i] = " " + spaces[i - 1];
         }
         String diamond = "";
-        for (int i = 0; i < maxSize; i ++) {
+        for (int i = 0; i < maxSize; i++) {
             diamond += spaces[spaces.length - i - 1] + asterisks[i] + "\n";
             }
-        for (int i = 1; i <maxSize; i ++) {
+        for (int i = 1; i < maxSize; i++) {
             diamond += spaces[i] + asterisks[asterisks.length - i - 1] + "\n";
             }
         stream.print(diamond);
