e83ce645322e68e0a538dec56b10549bb5c0ea26
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-20 01:15:17 +0300

    Update Diamond.java

--- src/main/java/core/basesyntax/Diamond.java
+++ src/main/java/core/basesyntax/Diamond.java
@@ -13,6 +13,25 @@ public class Diamond {
      * или stream.println()
      */
     public static void drawDiamond(int maxSize, PrintStream stream) {
+        for(int i = 0; i < maxSize; i++) {
+              for(int j = maxSize-2; j >= i; j--) {
+                  stream.print(" ");
+              }
+              for(int k = 0; k <= i; k++) {
+                  stream.print("* ");
+              }
+              stream.println();
+          }
+          
+          for(int i = 1; i < maxSize; i++) {
+              for(int k = 0; k <= i-1; k++) {
+                  stream.print(" ");
+              }
+              for(int j = maxSize-1; j >= i; j--) {
+                  stream.print("* ");
+              }
+              stream.println();
+          }
     }
 
     public static void main(String[] args) {
