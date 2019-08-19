7807cb95c9b0f72657c6e5e5beddd00c2b3ce576
RostykRosolovskyi <38504193+RostykRosolovskyi@users.noreply.github.com>
2019-08-19 19:42:29 +0300

    supplemented the method

--- src/main/java/core/basesyntax/Diamond.java
+++ src/main/java/core/basesyntax/Diamond.java
@@ -13,6 +13,33 @@ public class Diamond {
      * или stream.println()
      */
     public static void drawDiamond(int maxSize, PrintStream stream) {
+        int z = 1;
+        for (int i = 0; i < maxSize / 2 + 1; i++) {
+            int prob = (maxSize - z) / 2;
+
+            for (int j = 0; j < prob; j++) {
+                stream.print(" ");
+            }
+            for (int k = 0; k < z; k++) {
+                stream.print("*");
+            }
+            stream.println();
+
+            z = z + 2;
+        }
+        z = maxSize - 2;
+        //===
+        for (int i = maxSize / 2; i > 0; i--) {
+            int prob = (maxSize - z) / 2;
+            for (int j = 0; j < prob; j++) {
+                stream.print(" ");
+            }
+            for (int k = 0; k < z; k++) {
+                stream.print("*");
+            }
+            stream.println();
+            z = z - 2;
+        }
     }
 
     public static void main(String[] args) {
