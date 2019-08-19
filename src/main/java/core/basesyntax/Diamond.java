a9ec89cc87775c895076c6542223f46f26874676
RomanMinevich <52323566+RomanMinevich@users.noreply.github.com>
2019-08-19 18:37:40 +0300

    Complete task

--- src/main/java/core/basesyntax/Diamond.java
+++ src/main/java/core/basesyntax/Diamond.java
@@ -13,6 +13,27 @@ public class Diamond {
      * или stream.println()
      */
     public static void drawDiamond(int maxSize, PrintStream stream) {
+      if (n % 2 == 0 || n < 0) {
+      stream.println(null);
+    }
+    String[] asterisks = new String[n/2 + 1];
+    asterisks[0] = "*\n";
+    for(int i = 1; i < asterisks.length; i ++) {
+      asterisks[i] = "**" + asterisks[i - 1];
+    }
+    String[] spaces = new String[n/2 + 1];
+    spaces[0] = "";
+    for(int i = 1; i < spaces.length; i ++) {
+      spaces[i] = " " + spaces[i - 1];
+    }
+    String diamond = "";
+    for (int i = 0; i < n/2 + 1; i ++) {
+      diamond += spaces[spaces.length - i - 1] + asterisks[i];
+    }
+    for (int i = 1; i < n/2 + 1; i ++) {
+      diamond += spaces[i] + asterisks[asterisks.length - i - 1];
+    }
+    stream.println(diamond);
     }
 
     public static void main(String[] args) {
