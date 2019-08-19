1f1a92f58249b0551fb10ca6870dd7f96adedaf1
AndreyDMA <andreydmitriev@ukr.net>
2019-08-19 23:22:17 +0300

    Solution for Diamond

--- src/main/java/core/basesyntax/Diamond.java
+++ src/main/java/core/basesyntax/Diamond.java
@@ -6,19 +6,19 @@ public class Diamond {
 
     /**
      * Нарисовать ромб со звёздочек заданной величины.
-     *
      * Пример: величина 3, значит максимальное кол-во звёзд в ряду (по диагонали) будет 3.
-     *
      * Вместо System.out.print() или System.out.println() использовать stream.print()
      * или stream.println()
      */
     public static void drawDiamond(int maxSize, PrintStream stream) {
         String str = "";
 
-        for (int i = 1; i < maxSize; i += 1)
+        for (int i = 1; i < maxSize; i += 1) {
             str += " ".repeat(maxSize - i) + "* ".repeat(i) + "\n";
-        for (int i = maxSize; i >= 1; i -= 1)
+        }
+        for (int i = maxSize; i >= 1; i -= 1) {
             str += " ".repeat(maxSize - i) + "* ".repeat(i) + "\n";
+        }
         stream.print(str);
     }
 
