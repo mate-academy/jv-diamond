0c531605c266c1289380407b40a02639fedf738c
RostykRosolovskyi <38504193+RostykRosolovskyi@users.noreply.github.com>
2019-08-20 14:06:15 +0300

    supplemented the method

--- src/main/java/core/basesyntax/Diamond.java
+++ src/main/java/core/basesyntax/Diamond.java
@@ -2,43 +2,28 @@ package core.basesyntax;
 
 import java.io.PrintStream;
 
-public class Diamond {
+class Diamond {
 
     /**
      * Нарисовать ромб со звёздочек заданной величины.
-     *
      * Пример: величина 3, значит максимальное кол-во звёзд в ряду (по диагонали) будет 3.
-     *
      * Вместо System.out.print() или System.out.println() использовать stream.print()
      * или stream.println()
      */
     public static void drawDiamond(int maxSize, PrintStream stream) {
-        int z = 1;
-        for (int i = 0; i < maxSize / 2 + 1; i++) {
-            int prob = (maxSize - z) / 2;
+        for (int i = 0; i < 2 * maxSize; i++) {
+            int right = -Math.abs(i - maxSize) + 2 * maxSize - 1;
+            for (int x = 0; x <= right; x++) {
+                int low = Math.abs(x - maxSize + 1);
+                int top = -Math.abs(x - maxSize + 1) + 2 * maxSize - 2;
+                if (i >= low && i <= top && (i + x) % 2 != maxSize % 2) {
+                    stream.print("*");
+                } else {
+                    stream.print(" ");
+                }
 
-            for (int j = 0; j < prob; j++) {
-                stream.print(" ");
-            }
-            for (int k = 0; k < z; k++) {
-                stream.print("*");
             }
             stream.println();
-
-            z = z + 2;
-        }
-        z = maxSize - 2;
-        //===
-        for (int i = maxSize / 2; i > 0; i--) {
-            int prob = (maxSize - z) / 2;
-            for (int j = 0; j < prob; j++) {
-                stream.print(" ");
-            }
-            for (int k = 0; k < z; k++) {
-                stream.print("*");
-            }
-            stream.println();
-            z = z - 2;
         }
     }
 
