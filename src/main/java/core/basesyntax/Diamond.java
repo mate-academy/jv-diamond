62afa59cdb1da200f4867c0e508137039b64c712
Iurii <kenu21@gmail.com>
2019-08-20 16:46:49 +0300

    added hw 2 solution

--- src/main/java/core/basesyntax/Diamond.java
+++ src/main/java/core/basesyntax/Diamond.java
@@ -7,54 +7,40 @@ public class Diamond {
     /**
      * Нарисовать ромб со звёздочек заданной величины.
      *
-     * Пример: величина 3, значит максимальное кол-во звёзд в ряду (по диагонали) будет 3.
+     * <p></p>Пример: величина 3, значит максимальное кол-во звёзд в ряду (по диагонали) будет 3.
      *
-     * Вместо System.out.print() или System.out.println() использовать stream.print()
+     * <p></p>Вместо System.out.print() или System.out.println() использовать stream.print()
      * или stream.println()
      */
     public static void drawDiamond(int maxSize, PrintStream stream) {
-        int middleRhomb = maxSize / 2;
+        int count = maxSize - 1;
 
-        for (int i = 0; i < middleRhomb; i++) {
+        //Top
+        for (int i = 0; i < maxSize; i++) {
 
-            for (int j = middleRhomb; j > i; j--) {
+            for (int j = 0; j < count; j++) {
                 stream.print(" ");
             }
 
-            for (int j1 = 0; j1 <= i; j1++) {
-                stream.print("*");
+            for (int k = 0; k <= i; k++) {
+                stream.print("* ");
             }
-
-            for (int j = 0; j < i; j++) {
-                stream.print("*");
-            }
-
-            for (int j = middleRhomb; j > i; j--) {
-                stream.print(" ");
-            }
-
             stream.println();
+            count--;
         }
 
-        for (int i = middleRhomb; i >= 0; i--) {
-
-            for (int j = middleRhomb; j > i; j--) {
+        // Bottom
+        count = 1;
+        for (int i = maxSize - 1; i > 0; i--) {
+            for (int j = 0; j < count; j++) {
                 stream.print(" ");
             }
 
-            for (int j1 = 0; j1 <= i; j1++) {
-                stream.print("+");
+            for (int k = 0; k < i; k++) {
+                stream.print("* ");
             }
-
-            for (int j = 0; j < i; j++) {
-                stream.print("+");
-            }
-
-            for (int j = middleRhomb; j > i; j--) {
-                stream.print(" ");
-            }
-
             stream.println();
+            count++;
         }
     }
 
