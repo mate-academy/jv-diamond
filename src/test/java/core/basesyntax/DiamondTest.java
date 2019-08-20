b8d72797f917ae239b74d8113fe7b996e5e49988
Тарас <tarashryts@gmail.com>
2019-08-20 14:39:23 +0300

    add solusion too drawDiamond method

--- src/test/java/core/basesyntax/DiamondTest.java
+++ src/test/java/core/basesyntax/DiamondTest.java
@@ -54,7 +54,7 @@ public class DiamondTest {
         String[] actualResult = actualPrintStream.getPrintString().split("\n");
         String[] expectedResult = expected.split("\n");
         for (int i = 0; i < actualResult.length; i++) {
-            if (!actualResult[i].isEmpty()) {
+            if (!actualResult[i].isBlank()) {
                 actualResult[i] = actualResult[i].replaceAll("\\s+$", "");
                 Assert.assertEquals("Test failed with parameter: maxSize = " + maxSize,
                         expectedResult[i],
