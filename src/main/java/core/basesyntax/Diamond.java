946996d509b7857428da4b51a580590dba12601e
RostykRosolovskyi <38504193+RostykRosolovskyi@users.noreply.github.com>
2019-08-20 18:25:47 +0300

    supplemented the method

--- src/main/java/core/basesyntax/Diamond.java
+++ src/main/java/core/basesyntax/Diamond.java
@@ -12,14 +12,14 @@ class Diamond {
      */
     public static void drawDiamond(int maxSize, PrintStream stream) {
         for (int i = 0; i < 2 * maxSize; i++) {
-            int right = - Math.abs(i - maxSize) + 2 * maxSize - 1;
+            int right =  - Math.abs(i - maxSize) + 2 * maxSize - 1;
             for (int x = 0; x <= right; x++) {
                 int low = Math.abs(x - maxSize + 1);
                 int top = - Math.abs(x - maxSize + 1) + 2 * maxSize - 2;
                 if (i >= low && i <= top && (i + x) % 2 != maxSize % 2) {
                     stream.print("*");
                 } else {
-                    stream.print("  ");
+                    stream.print(" ");
                 }
 
             }
