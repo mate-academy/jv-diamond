6f6f88d5b293b6ce4447c962ddc66138bc234167
Koval <xui>
2019-08-21 13:50:10 +0300

    Complete hw1.2

--- src/main/java/core/basesyntax/Diamond.java
+++ src/main/java/core/basesyntax/Diamond.java
@@ -13,6 +13,24 @@ public class Diamond {
      * или stream.println()
      */
     public static void drawDiamond(int maxSize, PrintStream stream) {
+        if (maxSize <= 0) {
+            stream.println("maxSize can`t be negative");
+            return;
+        }
+        int spaces = maxSize - 1;
+        int stars = 1;
+
+        for (int i = 1; i < maxSize * 2; i++) {
+            for (int j = 0; j < spaces; j++) {
+                stream.print(" ");
+            }
+            for (int j = 0; j < stars; j++) {
+                stream.print("* ");
+            }
+            stream.println();
+            spaces = i < maxSize ? --spaces : ++spaces;
+            stars = i < maxSize ? ++stars : --stars;
+        }
     }
 
     public static void main(String[] args) {
